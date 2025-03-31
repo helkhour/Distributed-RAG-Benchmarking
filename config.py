17fd6bb4699c542380cda6f303a5e042c27228a0
helkhour <helenlkhoury@gmail.com>
2025-04-01 00:14:20 +0300

    lower from k=5 to k=3 increases precision > more accurate documents retrieved
    Retrieval Success: 100.00% (390/390 queries returned results)
    Average Precision: 73.93% (865/1170 relevant documents retrieved)

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ EMBEDDING_SIZE = 384  # Size for all-MiniLM-L6-v2
 DB_URI = "mongodb://localhost:32768/?directConnection=true"  # Adjust port when changing model !!
 DB_NAME = "rag_db"
 COLLECTION_NAME = "hotpotqa_docs"
-K = 5  # Top k results to retrieve
+K = 3  # Top k results to retrieve
 DATASET_NAME = "rungalileo/ragbench"
 SUBSET_NAME = "hotpotqa"
